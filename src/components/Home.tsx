4fcac832feccdd7047297a914638f1a44256c75d
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-08-27 12:56:28 +0530

    UI changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import { UserGreeting } from "./UserGreeting";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{alignItems:"center", flexDirection:'column', display:'flex'}}>
       <h2>This is home component</h2>
       <p data-testid="para">This is the paragraph in home page</p>
       <UserGreeting />
